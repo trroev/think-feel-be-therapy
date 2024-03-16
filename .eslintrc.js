9cd7a3a7314ea6e79e72f36dae6cf6cbfb62aef9
Trevor Mathiak <tmathiak@gmail.com>
2024-03-16 14:05:11 -0400

    build: add unused-imports plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const { type } = require("os");
+const { type } = require('os')
 
 module.exports = {
   root: true,
@@ -6,16 +6,16 @@ module.exports = {
     node: true,
     es6: true,
   },
-  parserOptions: { ecmaVersion: 8, sourceType: "module" },
-  ignorePatterns: ["node_modules/*"],
-  extends: ["eslint:recommended"],
+  parserOptions: { ecmaVersion: 8, sourceType: 'module' },
+  ignorePatterns: ['node_modules/*'],
+  extends: ['eslint:recommended'],
   overrides: [
     {
-      files: ["**/*.ts", "**/*.tsx"],
-      parser: "@typescript-eslint/parser",
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
       settings: {
-        react: { version: "detect" },
-        "import/resolver": {
+        react: { version: 'detect' },
+        'import/resolver': {
           typescript: {},
         },
       },
@@ -25,65 +25,76 @@ module.exports = {
         es6: true,
       },
       extends: [
-        "eslint:recommended",
-        "plugin:import/errors",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:prettier/recommended",
-        "plugin:@shopify/esnext",
-        "plugin:@shopify/react",
-        "plugin:@shopify/prettier",
-        "plugin:tailwindcss/recommended",
-        "next/core-web-vitals",
-        "plugin:storybook/recommended",
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@shopify/esnext',
+        'plugin:@shopify/react',
+        'plugin:@shopify/prettier',
+        'plugin:tailwindcss/recommended',
+        'next/core-web-vitals',
+        'plugin:unused-imports/recommended',
       ],
       rules: {
-        "no-restricted-imports": [
-          "error",
+        'no-restricted-imports': [
+          'error',
           {
-            patterns: ["@/features/*/*"],
+            patterns: ['@/features/*/*'],
           },
         ],
-        "linebreak-style": ["error", "unix"],
-        "react/prop-types": "off",
+        'linebreak-style': ['error', 'unix'],
+        'react/prop-types': 'off',
 
-        "import/order": [
-          "error",
+        'import/order': [
+          'error',
           {
             groups: [
-              "builtin",
-              "external",
-              "internal",
-              "parent",
-              "sibling",
-              "index",
-              "object",
+              'builtin',
+              'external',
+              'internal',
+              'parent',
+              'sibling',
+              'index',
+              'object',
             ],
-            "newlines-between": "always",
-            alphabetize: { order: "asc", caseInsensitive: true },
+            'newlines-between': 'always',
+            alphabetize: { order: 'asc', caseInsensitive: true },
           },
         ],
-        "import/default": "off",
-        "import/no-named-as-default-member": "off",
-        "import/no-named-as-default": "off",
+        'import/default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/no-named-as-default': 'off',
 
-        "react/react-in-jsx-scope": "off",
+        'react/react-in-jsx-scope': 'off',
 
-        "jsx-a11y/anchor-is-valid": "off",
+        'jsx-a11y/anchor-is-valid': 'off',
 
-        "@typescript-eslint/no-unused-vars": ["error"],
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            vars: 'all',
+            varsIgnorePattern: '^_',
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+          },
+        ],
 
-        "@typescript-eslint/explicit-function-return-type": ["off"],
-        "@typescript-eslint/explicit-module-boundary-types": ["off"],
-        "@typescript-eslint/no-empty-interface": ["off"],
-        "@typescript-eslint/no-explicit-any": ["off"],
+        '@typescript-eslint/no-unused-vars': ['error'],
 
-        "prettier/prettier": ["error", {}, { usePrettierrc: true }],
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        '@typescript-eslint/no-empty-interface': ['off'],
+        '@typescript-eslint/no-explicit-any': ['off'],
+
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
       },
     },
   ],
-};
+}

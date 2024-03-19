d1e681e28274eb33ba5e4a3b96857ab43ac02f46
Trevor Mathiak <tmathiak@gmail.com>
2024-03-19 10:06:22 -0400

    refactor: center text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center">
-      <h1>Think.Feel.Be. Therapy</h1>
+      <h1 className="text-center">Think.Feel.Be. Therapy</h1>
       <h4 className="mt-2">Coming soon...</h4>
     </div>
   )

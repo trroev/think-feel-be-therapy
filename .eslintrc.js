2b7ac759b3217e634a23d232e1bd9d43c2fd3730
Trevor Mathiak <tmathiak@gmail.com>
2024-03-26 23:22:23 -0400

    build: eslint changes and add radix slot

--- .eslintrc.js
+++ .eslintrc.js
@@ -69,7 +69,11 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': ['off'],
         '@typescript-eslint/explicit-module-boundary-types': ['off'],
         '@typescript-eslint/no-empty-interface': ['off'],
-        '@typescript-eslint/no-explicit-any': ['off'],
+        '@typescript-eslint/no-explicit-any': ['error'],
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+        ],
       },
     },
   ],

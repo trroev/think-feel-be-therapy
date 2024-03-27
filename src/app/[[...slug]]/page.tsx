7e85aaf5d2d3a798b5b31b3d59d4ab8c8fc5b455
Trevor Mathiak <tmathiak@gmail.com>
2024-03-26 20:08:47 -0400

    feat: add builder.io

---
+++ src/app/[[...slug]]/page.tsx
@@ -0,0 +1,43 @@
+import { BuilderComponents } from '@/components'
+import { BUILDER_API_KEY } from '@/config'
+import { Content, fetchOneEntry, isEditing, isPreviewing } from '@builder.io/sdk-react/edge'
+
+interface PageProps {
+  params: {
+    slug: string[]
+  }
+  searchParams: Record<string, string>
+}
+
+export default async function Page(props: PageProps) {
+  const urlPath = '/' + (props.params?.slug?.join('/') || '')
+
+  const content = await fetchOneEntry({
+    options: props.searchParams,
+    apiKey: BUILDER_API_KEY,
+    model: 'page',
+    userAttributes: { urlPath },
+  })
+
+  const canShowContent =
+    content || isPreviewing(props.searchParams) || isEditing(props.searchParams)
+
+  if (!canShowContent) {
+    return (
+      <>
+        <h1>404</h1>
+        <p>Make sure you have your content published at builder.io.</p>
+      </>
+    )
+  }
+  return (
+    <div className="container relative flex h-screen flex-col items-center justify-center">
+      <Content
+        content={content}
+        apiKey={BUILDER_API_KEY}
+        model={'page'}
+        customComponents={BuilderComponents}
+      />
+    </div>
+  )
+}

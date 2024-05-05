89fcee2d91299629c2381eae7d95529b50f27364
Trevor Mathiak <tmathiak@gmail.com>
2024-05-05 14:15:55 -0400

    update toast imports

--- src/components/ui/toast/toaster.tsx
+++ src/components/ui/toast/toaster.tsx
@@ -7,8 +7,7 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from '@/components/ui/toast'
-
+} from './toast'
 import { useToast } from './use-toast'
 
 export const Toaster = () => {

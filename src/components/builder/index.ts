1802c5f3aa82265e28fcff39b0c0344dfacf3d6c
Trevor Mathiak <tmathiak@gmail.com>
2024-05-11 22:31:28 -0400

    update builder and imports

--- src/components/builder/index.ts
+++ src/components/builder/index.ts
@@ -1,67 +1,7 @@
 'use client'
 
-import dynamic from 'next/dynamic'
 import { type RegisteredComponent } from '@builder.io/sdk-react'
 
-export const BuilderComponents: RegisteredComponent[] = [
-  {
-    component: dynamic(() => import('@/components').then((mod) => mod.FAQ), {
-      ssr: false,
-    }),
-    name: 'FAQs',
-    inputs: [
-      {
-        name: 'items',
-        type: 'list',
-        subFields: [
-          // {
-          //   name: 'value',
-          //   type: 'string',
-          //   required: true,
-          // },
-          {
-            name: 'question',
-            type: 'longText',
-            required: true,
-          },
-          {
-            name: 'answer',
-            type: 'longText',
-            required: true,
-          },
-        ],
-      },
-      {
-        name: 'type',
-        type: 'string',
-        defaultValue: 'multiple',
-        enum: ['single', 'multiple'],
-      },
-    ],
-  },
-  {
-    component: dynamic(() => import('@/components').then((mod) => mod.Button), {
-      ssr: false,
-    }),
-    name: 'Button',
-    inputs: [
-      {
-        name: 'variant',
-        type: 'string',
-        defaultValue: 'default',
-        enum: ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'],
-      },
-      {
-        name: 'size',
-        type: 'string',
-        defaultValue: 'default',
-        enum: ['default', 'sm', 'lg', 'icon'],
-      },
-      {
-        name: 'buttonText',
-        type: 'string',
-        required: true,
-      },
-    ],
-  },
-]
+import { FAQRegistration } from '@/components/faq'
+
+export const BuilderComponents: RegisteredComponent[] = [FAQRegistration]

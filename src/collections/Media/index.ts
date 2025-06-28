9144a5301b67b912386905ece42829d00990466e
Trevor Mathiak <tmathiak@gmail.com>
2025-06-28 10:36:04 -0400

    chore: remove unused imports and imageSizes configuration from Media collection in PayloadCMS for cleaner code

--- src/collections/Media/index.ts
+++ src/collections/Media/index.ts
@@ -1,5 +1,3 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 import {
   FixedToolbarFeature,
   InlineToolbarFeature,
@@ -9,9 +7,6 @@ import type { CollectionConfig } from 'payload'
 
 import { anyone } from '@/access/anyone'
 import { authenticated } from '@/access/authenticated'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   access: {
@@ -42,42 +37,7 @@ export const Media: CollectionConfig = {
   ],
   slug: 'media',
   upload: {
-    // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
-    staticDir: path.resolve(dirname, '../../public/media'),
     adminThumbnail: 'thumbnail',
     focalPoint: true,
-    imageSizes: [
-      {
-        name: 'thumbnail',
-        width: 300,
-      },
-      {
-        name: 'square',
-        width: 500,
-        height: 500,
-      },
-      {
-        name: 'small',
-        width: 600,
-      },
-      {
-        name: 'medium',
-        width: 900,
-      },
-      {
-        name: 'large',
-        width: 1400,
-      },
-      {
-        name: 'xlarge',
-        width: 1920,
-      },
-      {
-        name: 'og',
-        width: 1200,
-        height: 630,
-        crop: 'center',
-      },
-    ],
   },
 }
